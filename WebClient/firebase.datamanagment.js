85126773a65f0d0d65b5ce582855fbbd06e4437c
Yura <fogolan@gmail.com>
2017-07-14 00:39:39 +0300

    formatted files

--- WebClient/firebase.datamanagment.js
+++ WebClient/firebase.datamanagment.js
@@ -1,5 +1,5 @@
- function initializeFirebase() {
- // Initialize Firebase
+function initializeFirebase() {
+    // Initialize Firebase
     var config = {
         apiKey: "AIzaSyCTi9F3o1gl6ytvOzOqp20vjXX7omDstGU",
         authDomain: "win10-and-web-app.firebaseapp.com",
@@ -10,31 +10,31 @@
     };
     firebase.initializeApp(config);
     return firebase.database();
- }
-    function jsonToList(data, el) {
-        clearList(el);
-        $.each(data, function (key, val) {
-            $(el).append($('<li>').text(val.Field1 + ' ' + val.Field2 + ' ' + val.Field3));
-        });
-    }
+}
+function jsonToList(data, el) {
+    clearList(el);
+    $.each(data, function (key, val) {
+        $(el).append($('<li>').text(val.Field1 + ' ' + val.Field2 + ' ' + val.Field3));
+    });
+}
 
-    function ListToJson(el, separator) {
-        items = [];
-        $(el).children().each(function () {
-            var $this = $(this);
-            var objectFields = $this.html().split(separator);
-            var item = { Field1: objectFields[0], Field2: objectFields[1], Field3: objectFields[2] };
-            items.push(item);
-        });
-        return items;
-    }
+function ListToJson(el, separator) {
+    items = [];
+    $(el).children().each(function () {
+        var $this = $(this);
+        var objectFields = $this.html().split(separator);
+        var item = { Field1: objectFields[0], Field2: objectFields[1], Field3: objectFields[2] };
+        items.push(item);
+    });
+    return items;
+}
 
-    function clearList(el) {
-        $.each(data, function (key, val) {
-            $(el + ' li').remove();
-        });
-    }
+function clearList(el) {
+    $.each(data, function (key, val) {
+        $(el + ' li').remove();
+    });
+}
 
-    function setToDatabase(database, data) {
-        database.ref().set(data);
-    }
+function setToDatabase(database, data) {
+    database.ref().set(data);
+}
